d70dd7ec89fb74f24d8f9da5e7c9826d1eb925d4
Davidsusairaj s <davidsusairaj.sebasthiyar@solverminds.com>
2024-11-01 18:23:12 +0530

    create and delete bulk data completed

--- routers/feedbackRouters.js
+++ routers/feedbackRouters.js
@@ -5,9 +5,9 @@ const  router = express.Router();
 const {createTask,getTasks,getSingleTask,updateSingleTask,deleteSingleTask,createTasks,updateTasks,deleteTasks} = require("../controllers/feedbackControllers");
 router.post("/",createTask);
 router.get("/",getTasks)
-router.get("/:id",getSingleTask)
-router.patch("/:id",updateSingleTask)
-router.delete("/:id",deleteSingleTask)
+// router.get("/:id",getSingleTask)
+// router.patch("/:id",updateSingleTask)
+// router.delete("/:id",deleteSingleTask)
 
 router.post("/bulk", createTasks);       // For bulk creation
 router.patch("/bulk", updateTasks);      // For bulk update
